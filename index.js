84312540cff886d45aa17e205ceaacffde1a3356
Melanie Uba <melanie.uba@gmail.com>
2017-12-03 23:38:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b) {
+  return a+b; 
+}
+function subtract (a,b) {
+  return a-b
+} 
+function multiply  (a,b) {
+  return a*b
+}
+function divide(a,b) {
+  return a/b
+}
+function inc(a){
+  return ++a
+}
+function dec(a){
+  return --a
+}
+function makeInt(a){
+ parseInt('a') 
+ parseInt(10)
+ parseInt('nonsense!', 10)
+ return parseInt(a)
+}
+function preserveDecimal(a){
+  parseFloat('a')
+  parseFloat('nonsense!')
+  return parseFloat(a)
+}
